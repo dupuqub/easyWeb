2f919912f3b357fdff5834e4af81d19631ca6028
Fillipe Ogg <dupuqub@gmail.com>
2019-06-01 23:47:26 -0300

    complete solution to carousel

--- js/index.js
+++ js/index.js
@@ -9,18 +9,20 @@ const arrayer = length => Array.from({length})
 const frames = getAll(`.imgCarousel`)
 let next = 0
 
-console.log(`ok`)
-
 setInterval(() => {
   if(next > frames.length - 2) next = 0
   else next ++
 
-  console.log(next)
-
   frames.forEach((frame, index) => {
+    if(index < next - 1 && index
+    || next === 2 && !index
+    || !next && index === frames.length - 2
+    || next === 1 && index === frames.length - 1) {
+      frame.style.transition = `all 0s`
+      setTimeout(() => frame.style.transition = `all 2s`, 1000)
+    }
 
     let mod =
-
         !next && index === frames.length - 1
       ? -frames.length
       : index < next - 1
